79f7a0725578699dd565ae5efd5974a5527ddb2c
Anton <danilovanton724@gmail.com>
2024-03-30 14:21:07 +0100

    Initial Commit

---
+++ Joker APp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Joker APp
+//
+//  Created by Anton on 30/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Joker APp/Joker_APpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joker_APpApp.swift
+//  Joker APp
+//
+//  Created by Anton on 30/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Joker_APpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
